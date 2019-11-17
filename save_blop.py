22d3f09067f145e952586b3aa84c3e6ab297f41a
Caaraya <christina-araya@hotmail.com>
2019-11-17 20:57:13 +0000

    cleanup, add way to set the figure size

--- save_blop.py
+++ save_blop.py
@@ -3,23 +3,24 @@ import numpy as np
 from gen_blop import *
 import math
 
-def save_blop(np_size=7, rand_points=7, scale=1, rad=0.2, edgy=0.05, name="blops"):
-  fig, ax = plt.subplots()
+def get_quadranted_array(nodes):
+    r,l = 0,0
+    nearest_sq = round(math.sqrt(nodes))
+    arr = []
+    for i in range(nodes):
+      arr.append([l,r])
+      if (r == nearest_sq - 1):
+        l += 1
+        r = 0
+      else:
+        r += 1
+    return arr
+
+def save_blop(fig_size=15, np_size=7, rand_points=7, scale=1, rad=0.2, edgy=0.05, name="blops"):
+  fig, ax = plt.subplots(figsize=(fig_size, fig_size))
   ax.set_aspect("equal")
 
-  r,l = 0,0
-  nearest_sq = round(math.sqrt(np_size))
-  arr = []
-  for i in range(np_size):
-    arr.append([l,r])
-    if (r == nearest_sq - 1):
-      l += 1
-      r = 0
-    else:
-      r += 1
-  print(arr)
-
-  for c in arr:
+  for c in get_quadranted_array(np_size):
 
       a = get_random_points(n=rand_points, scale=scale) + c
       x,y, _ = get_bezier_curve(a,rad=rad, edgy=edgy)
