5bbb6ecc8d14090d1a7a93e3af890b41ffd02eae
Caaraya <christina-araya@hotmail.com>
2019-11-17 22:04:29 +0000

    added json output for center along with the "ratio" or number of columns, removed center plot

--- save_blop.py
+++ save_blop.py
@@ -2,17 +2,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 from gen_blop import *
 import math
+import json
 
 def avg(arr):
   return sum(arr) / len(arr)
 
+def nearest_sq(n):
+  return round(math.sqrt(n))
+
 def get_quadranted_array(nodes):
     r,l = 0,0
-    nearest_sq = round(math.sqrt(nodes))
+    n_sq = nearest_sq(nodes)
     arr = []
     for i in range(nodes):
       arr.append([l,r])
-      if (r == nearest_sq - 1):
+      if (r == n_sq - 1):
         l += 1
         r = 0
       else:
@@ -22,6 +26,9 @@ def get_quadranted_array(nodes):
 def save_blop(np_size=7, rand_points=7, scale=1, rad=0.2, edgy=0.05, name="blops"):
   fig, ax = plt.subplots()
   ax.set_aspect("equal")
+  data = {}
+  data['ratio'] = nearest_sq(np_size)
+  data['center'] = []
 
   for c in get_quadranted_array(np_size):
 
@@ -29,7 +36,10 @@ def save_blop(np_size=7, rand_points=7, scale=1, rad=0.2, edgy=0.05, name="blops
       x,y, _ = get_bezier_curve(a,rad=rad, edgy=edgy)
       x_mid, y_mid = avg(x), avg(y)
       plt.fill(x,y)
-      plt.plot([x_mid], [y_mid], color='k',marker='o',lw=0, linestyle="")
+      data['center'].append([x_mid,y_mid])
+      #plt.plot([x_mid], [y_mid], color='k',marker='o',lw=0, linestyle="")
 
   plt.axis('off')
   plt.savefig(name + ".png", bbox_inches='tight', transparent=True)
+  with open(name + ".json", 'w') as outfile:
+    json.dump(data, outfile)
