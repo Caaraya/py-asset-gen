d8e83028a4b20a2614ef73bfc50d2f5129d7f9dc
Caaraya <christina-araya@hotmail.com>
2019-12-02 01:19:47 +0000

    added more generated files with different names and amounts of pivot points

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from save_blop import *
 
 save_blop(np_size=30, rand_points=9, scale=0.95)
+save_blop(np_size=30, rand_points=6, scale=0.95, pivot_points=2, name="two_piv_blops")
+save_blop(np_size=30, rand_points=4, scale=0.95, pivot_points=1, name="one_piv_blops")
--- save_blop.py
+++ save_blop.py
@@ -4,6 +4,15 @@ from gen_blop import *
 import math
 import json
 
+def get_pivot_points(x,y,piv):
+  pivx = round(len(x)/piv)
+  pivy = round(len(y)/piv)
+  data_points = []
+  for i in range(piv):
+    data_points.append([x[i*pivx], y[i*pivy]])
+  return data_points
+
+
 def avg(arr):
   return sum(arr) / len(arr)
 
@@ -23,7 +32,7 @@ def get_quadranted_array(nodes):
         r += 1
     return arr
 
-def save_blop(np_size=7, rand_points=7, scale=1, rad=0.2, edgy=0.05, name="blops"):
+def save_blop(np_size=7, rand_points=7, scale=1, rad=0.2, edgy=0.05, name="blops", pivot_points=5):
   fig, ax = plt.subplots()
   ax.set_aspect("equal")
   data = {}
@@ -34,9 +43,12 @@ def save_blop(np_size=7, rand_points=7, scale=1, rad=0.2, edgy=0.05, name="blops
       a = get_random_points(n=rand_points, scale=scale) + c
       x,y, _ = get_bezier_curve(a,rad=rad, edgy=edgy)
       x_mid, y_mid = avg(x), avg(y)
+      pivots = get_pivot_points(x,y,pivot_points);
       plt.fill(x,y)
-      data['shape'].append({'center': [x_mid,y_mid], 'quadrant': c})
+      data['shape'].append({'center': [x_mid,y_mid], 'quadrant': c, 'pivots': pivots})
       #plt.plot([x_mid], [y_mid], color='k',marker='o',lw=0, linestyle="")
+      #for p in pivots:
+      #  plt.plot([p[0]], [p[1]], color='w', marker='o', lw = 0, linestyle="")
 
   plt.axis('off')
   plt.savefig(name + ".png", bbox_inches='tight', transparent=True)
